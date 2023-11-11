cbf7f539d67b55bd3559dce8a05d311b285267d4
Magomed Alimkhanov <scrythepvp@gmail.com>
2023-11-11 01:16:06 +0100

    feat: add start screen

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import "./style.css";
 import Game from "./game";
+// import PlayIconPressed from "./playIconPressed.png";
 
 const game = new Game();
 
 document.querySelector("#start")?.addEventListener("click", () => {
+  game.startGame();
+});
+
+const startGameSec: HTMLTableSectionElement | null =
+  document.querySelector("#startGame");
+document.querySelector("#playIcon")?.addEventListener("click", () => {
+  startGameSec!.style.display = "none";
   game.startGame();
 });
 
