ad5b0a1d4f73e51fb1237a4ee3d82baf914433ed
Magomed Alimkhanov <scrythepvp@gmail.com>
2023-11-12 18:09:08 +0100

    feat: add ios support for device motion + refactor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import "./style.css";
 import Game from "./game";
 // import PlayIconPressed from "./playIconPressed.png";
+//
 interface ExtendedScreenOrientation extends ScreenOrientation {
   lock(orientation: string): Promise<void>;
 }
+
+interface DeviceOrientationEventiOS extends DeviceOrientationEvent {
+  requestPermission?: () => Promise<"granted" | "denied">;
+}
+
 const main = document.querySelector("main")!;
 const player: HTMLImageElement = document.querySelector("#player")!;
 const flyFuelBar: HTMLDivElement = document.querySelector("#fly-fuel div")!;
@@ -12,14 +18,7 @@ const game = new Game(main, player, flyFuelBar);
 
 const startGameSec: HTMLTableSectionElement =
   document.querySelector("#startGame")!;
-document.querySelector("#playIcon")?.addEventListener("click", () => {
-  startGameSec!.style.display = "none";
-  document.querySelector("html")?.requestFullscreen();
-  (screen.orientation as ExtendedScreenOrientation)
-    .lock("landscape")
-    .then(() => game.start())
-    .catch(() => game.start());
-});
+document.querySelector("#playIcon")?.addEventListener("click", startGame);
 
 document.querySelector("#pause")?.addEventListener("click", () => {
   game.pause();
@@ -32,4 +31,21 @@ document.querySelector("#stop")?.addEventListener("click", () => {
   game.gameOver();
 });
 
-addEventListener("deviceorientation", (e) => game.input(e));
+function startGame() {
+  startGameSec!.style.display = "none";
+  document.querySelector("html")?.requestFullscreen();
+  (screen.orientation as ExtendedScreenOrientation)
+    .lock("landscape")
+    .then(() => game.start())
+    .catch(() => game.start());
+
+  const requestPermission = (
+    DeviceOrientationEvent as unknown as DeviceOrientationEventiOS
+  ).requestPermission;
+  if (typeof requestPermission === "function") {
+    requestPermission().then((response: string) => {
+      if (response == "granted")
+        addEventListener("deviceorientation", (e) => game.input(e));
+    });
+  } else addEventListener("deviceorientation", (e) => game.input(e));
+}
